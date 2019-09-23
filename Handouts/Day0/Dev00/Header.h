8fe635c28e681a73cf65ece8954377250923de9c
pablogalve <eduinvestor100@gmail.com>
2019-09-23 18:50:55 +0200

    save work with two functions created

--- Handouts/Day0/Dev00/Header.h
+++ Handouts/Day0/Dev00/Header.h
@@ -1,6 +1,7 @@
 #ifndef _Header_H
 #define _Header_H
 using namespace std;
+#include <math.h>
 
 template <class T>
 class vec3 {
@@ -13,6 +14,10 @@ public:
 	vec3();
 	vec3(T, T, T);
 	~vec3();
+	void normalize();
+	void zero();
+	bool is_zero();
+	float distance_to(vec3);
 };
 
 template<class T>
@@ -21,8 +26,28 @@ vec3<T>::vec3() {}
 template<class T>
 vec3<T>::~vec3() {}
 
+
+
 template<class T>
 vec3<T>::vec3(T tx, T ty, T tz) {}
 
+template<class T>
+bool vec3<T>::is_zero() 
+{
+	if (x == 0 && y == 0 && z == 0)
+		return true;
+	else
+		return false;
+}
+
+template<class T>
+float vec3<T>::distance_to(vec3) 
+{
+	float distance;
+
+	distance = sqrt(pow(x - vec3, 2) + pow(y - vec3, 2) + pow(z - vec3, 2));
+
+	return distance;
+}
 
 #endif // !_Header
